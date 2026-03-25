ac31a418e7e37a020f0f8b41182a48bb67f414c4
Mohommad Redad Mehdi <mxm1684@case.edu>
2026-03-25 11:10:55 -0400

    Fix documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'OntoCheck'
 copyright = '2025, Rishabh Kundu, Van Tran, Redad Mehdi'
 author = 'Rishabh Kundu, Van Tran, Redad Mehdi'
-release = '0.0.1'
+release = '0.0.3.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
