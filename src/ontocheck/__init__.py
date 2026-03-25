49ccbb2828d3b829284b8aa18a8bd7b209a4f136
Mohommad Redad Mehdi <mxm1684@case.edu>
2026-03-25 11:19:59 -0400

    fixed the init.py

--- src/ontocheck/__init__.py
+++ src/ontocheck/__init__.py
@@ -13,10 +13,10 @@ from .semanticConnection import mainSemanticConnection_v_0_0_1
 from .mds_design_check import mds_design_check_v_0_0_1
 from .spell_check import spell_check_v_0_0_1
 from .run_assessment import run_ontology_assessment
-from check_class_name_capital import mainClassNameCapitalCheck_v_0_0_1
-from check_class_name_space import mainClassNameSpaceCheck_v_0_0_1
-from check_label import mainLabelCheck_v_0_0_1
-from class_search import mainClassSearch_v_0_0_1
+from .check_class_name_capital import mainClassNameCapitalCheck_v_0_0_1
+from .check_class_name_space import mainClassNameSpaceCheck_v_0_0_1
+from .check_label import mainLabelCheck_v_0_0_1
+from .class_search import mainClassSearch_v_0_0_1
 
 __all__ = [
     "mainAltLabelCheck_v_0_0_1",
