f8cc9ae5b9489cfc29b6269c71ee9694e2dd93e3
Mohommad Redad Mehdi <mxm1684@case.edu>
2026-03-25 11:25:11 -0400

    Fixing the wrapper function.

--- src/ontocheck/run_assessment.py
+++ src/ontocheck/run_assessment.py
@@ -16,10 +16,10 @@ from .leafNodeCheck import mainLeafNodeCheck_v_0_0_1
 from .semanticConnection import mainSemanticConnection_v_0_0_1
 from .mds_design_check import mds_design_check_v_0_0_1
 from .spell_check import spell_check_v_0_0_1
-from check_class_name_capital import mainClassNameCapitalCheck_v_0_0_1
-from check_class_name_space import mainClassNameSpaceCheck_v_0_0_1
-from check_label import mainLabelCheck_v_0_0_1
-from class_search import mainClassSearch_v_0_0_1
+from .check_class_name_capital import mainClassNameCapitalCheck_v_0_0_1
+from .check_class_name_space import mainClassNameSpaceCheck_v_0_0_1
+from .check_label import mainLabelCheck_v_0_0_1
+from .class_search import mainClassSearch_v_0_0_1
 
 METRIC_DISPATCHER = {
     "altLabelCheck": mainAltLabelCheck_v_0_0_1,
